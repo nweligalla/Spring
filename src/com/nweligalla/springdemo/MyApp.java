a8079a81e34b6db6a55be3d8b3b4ab3d3131a19a
nweligalla <nayanaweligalla@gmail.com>
2021-04-07 12:41:29 +0530

    Initial Commit and added xml Config

---
+++ src/com/nweligalla/springdemo/MyApp.java
@@ -0,0 +1,15 @@
+package com.nweligalla.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		//create a object
+		Coach coach =new TrackCoach();
+		
+		//use the object
+		System.out.println(coach.getDailyWorkout());
+
+	}
+
+}
